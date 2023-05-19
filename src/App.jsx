0f104a27ef6f4b972fc451334cfeb11be86e427c
Lucas_Aballay <lucas.aballay.2000@gmail.com>
2023-05-19 13:23:42 -0300

    instalado react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Navbar></Navbar>
-      <ItemListContainer
-        greeting="Bienvenido a PokemOnion el sition donde podras comprar pokemones rescatados de manera 100% legal 😉"
-        bgColor="white"
-      ></ItemListContainer>
-      <ItemDetailContainer></ItemDetailContainer>
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <ItemListContainer
+          greeting="Bienvenido a PokemOnion el sition donde podras comprar pokemones rescatados de manera 100% legal 😉"
+          bgColor="white"
+        ></ItemListContainer>
+        <ItemDetailContainer></ItemDetailContainer>
+      </BrowserRouter>
     </>
   );
 }
