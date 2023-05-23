90b0d0ec303a6e337b63919a60839c8e6a800b0d
Lucas_Aballay <lucas.aballay.2000@gmail.com>
2023-05-23 18:09:00 -0300

    separado el itemDetail de su Container

---
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -0,0 +1,40 @@
+import ItemCount from "../ItemCount/ItemCount";
+import { useParams, Link } from "react-router-dom";
+import "./ItemDetail.css";
+import pokemons from "../../data/pokemons";
+import { useEffect, useState } from "react";
+
+function getPokeData(id) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const requestedPokemon = pokemons.find((item) => item.id === Number(id));
+
+      resolve(requestedPokemon);
+    }, 1000);
+  });
+}
+
+function ItemDetail() {
+  const [pokemon, setPokemon] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    getPokeData(id).then((respuesta) => {
+      setPokemon(respuesta);
+    });
+  }, []);
+  return (
+    <div className="itemDetail">
+      <Link to="/">
+        <button className="detailCloseBtn">X</button>
+      </Link>
+      <h2 className="detailTitle">{pokemon.name}</h2>
+      <p className="detailType">Type: {pokemon.category}</p>
+      <p className="detailInfo">{pokemon.info}</p>
+      <p className="detailPrice">$:{pokemon.price}</p>
+      <img className="detailImg" src={pokemon.img} alt={pokemon.name} />
+      <ItemCount stock={4} />
+    </div>
+  );
+}
+export default ItemDetail;
