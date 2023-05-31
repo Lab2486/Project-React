d463da53edd59d17eb6256ab1d8e5b2a1510a19b
Lucas_Aballay <lucas.aballay.2000@gmail.com>
2023-05-31 15:44:43 -0300

    a

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,8 +1,43 @@
+import { useEffect, useState } from "react";
+import pokemons from "../../data/pokemons";
 import "./ItemDetailContainer.css";
+import ItemCount from "../ItemCount/ItemCount";
+import { useParams, Link } from "react-router-dom";
 
-import ItemDetail from "../ItemDetail/ItemDetail";
+function getPokeData(id) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const requestedPokemon = pokemons.find((item) => item.id === Number(id));
+
+      resolve(requestedPokemon);
+    }, 1000);
+  });
+}
 
 function ItemDetailContainer() {
-  return <div className="detailContainer"></div>;
+  const [pokemon, setPokemon] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    getPokeData(id).then((respuesta) => {
+      setPokemon(respuesta);
+    });
+  }, []);
+
+  return (
+    <div className="detailContainer">
+      <div className="itemDetail">
+        <Link to="/">
+          <button className="detailCloseBtn">X</button>
+        </Link>
+        <h2 className="detailTitle">{pokemon.name}</h2>
+        <p className="detailType">Tipe: {pokemon.category}</p>
+        <p className="detailInfo">{pokemon.info}</p>
+        <p className="detailPrice">$:{pokemon.price}</p>
+        <img className="detailImg" src={pokemon.img} alt={pokemon.name} />
+        <ItemCount stock={4} />
+      </div>
+    </div>
+  );
 }
 export default ItemDetailContainer;
